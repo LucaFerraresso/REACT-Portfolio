75d508220e831bfe5271c8e4d289e129fcb7834b
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-08-09 20:29:04 +0200

    logout issue 3

--- MyApp/src/useContext/AuthContext.jsx
+++ MyApp/src/useContext/AuthContext.jsx
@@ -5,8 +5,16 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null); // Stato dell'utente
 
+  const login = (username) => {
+    setUser(username); // Imposta l'username come utente loggato
+  };
+
+  const logout = () => {
+    setUser(null); // Rimuovi l'utente loggato
+  };
+
   return (
-    <AuthContext.Provider value={{ user, setUser }}>
+    <AuthContext.Provider value={{ user, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
