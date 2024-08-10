23382cd638530201066064fdd49197afac1ef46f
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-08-10 12:04:39 +0200

    add skeletonizer

--- MyApp/src/pages/ExpensesChartComponent.jsx
+++ MyApp/src/pages/ExpensesChartComponent.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { getExpensesFirestore } from "../API/firestore";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 
 const ExpensesChart = () => {
   const [data, setData] = useState([]);
