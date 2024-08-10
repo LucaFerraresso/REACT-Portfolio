5356fe966d34714562abd67319b7a1e41e03f268
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-08-10 17:45:51 +0200

    fix build error

--- MyApp/src/API/firebaseAuth.js
+++ MyApp/src/API/firebaseAuth.js
@@ -2,7 +2,7 @@ import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
-export const auth = getAuth();
+const auth = getAuth();
 
 export async function loginUser(email, password) {
   try {
--- MyApp/src/useContext/AuthContext.jsx
+++ MyApp/src/useContext/AuthContext.jsx
@@ -1,6 +1,6 @@
 import React, { createContext, useContext, useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../API/firebaseAuth"; // Assicurati che il percorso sia corretto
+//import { auth } from "../API/firebaseAuth"; // Assicurati che il percorso sia corretto
 
 const AuthContext = createContext();
 
