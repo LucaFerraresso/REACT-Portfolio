c5685bf721f905ba77bea120246b0c18e5be61e5
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-08-11 12:33:18 +0200

    last try

--- MyApp/src/pages/Login.jsx
+++ MyApp/src/pages/Login.jsx
@@ -1,19 +1,20 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { loginUser, registerUser } from "../API/firebaseAuth";
-import { toast } from "react-toastify";
-import { useAuth } from "../useContext/AuthContext";
-import { Flipper, Flipped } from "react-flip-toolkit";
+import { loginUser, registerUser } from "../API/firebaseAuth"; // Import delle funzioni per login e registrazione
+import { toast } from "react-toastify"; // Import per notifiche
+import { useAuth } from "../useContext/AuthContext"; // Import del contesto di autenticazione
 
 const Login = () => {
+  // Stati per email, password e se si sta registrando
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isRegistering, setIsRegistering] = useState(false);
-  const navigate = useNavigate();
-  const { user, logout } = useAuth();
+  const navigate = useNavigate(); // Hook per navigare
+  const { user, logout } = useAuth(); // Hook per ottenere lo stato utente e il logout
 
+  // Funzione per gestire l'autenticazione
   const handleAuth = async (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Previene il comportamento predefinito del form
 
     // Regex per validare l'email
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -22,11 +23,13 @@ const Login = () => {
     const passwordRegex =
       /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/;
 
+    // Validazione email
     if (!emailRegex.test(email)) {
       toast.error("Email non valida, inserisci una email valida.");
       return;
     }
 
+    // Validazione password
     if (!passwordRegex.test(password)) {
       toast.error(
         "Password non valida! Deve contenere almeno 6 caratteri, un numero e un carattere speciale."
@@ -37,13 +40,14 @@ const Login = () => {
     try {
       let authenticatedUser;
 
+      // Logica di autenticazione
       if (isRegistering) {
         authenticatedUser = await registerUser(email, password);
         if (authenticatedUser) {
           toast.success(
             "Registrazione effettuata con successo! Login Automatico."
           );
-          setIsRegistering(false);
+          setIsRegistering(false); // Passa alla modalità login
         } else {
           toast.error("Registrazione fallita. Riprova.");
         }
@@ -51,22 +55,23 @@ const Login = () => {
         authenticatedUser = await loginUser(email, password);
         if (authenticatedUser) {
           toast.success("Login effettuato con successo!");
-          navigate("/homepage");
+          navigate("/homepage"); // Naviga alla homepage dopo il login
         } else {
           toast.error("Login fallito. Controlla le tue credenziali.");
         }
       }
     } catch (error) {
       toast.error("Errore durante l'operazione. Riprova.");
-      console.error("Errore:", error.message);
+      console.error("Errore:", error.message); // Logga l'errore
     }
   };
 
+  // Funzione per gestire il logout
   const handleLogout = async () => {
     try {
       await logout();
       toast.success("Logout effettuato con successo!");
-      navigate("/login");
+      navigate("/login"); // Naviga alla pagina di login dopo il logout
     } catch (error) {
       toast.error("Errore durante il logout: " + error.message);
       console.error("Errore durante il logout:", error);
@@ -75,119 +80,61 @@ const Login = () => {
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      {!user ? (
-        <Flipper flipKey={isRegistering} spring="noWobble">
-          <Flipped flipId="form">
-            <div
-              className="bg-white p-8 rounded shadow-md w-80 transition-transform duration-500 ease-in-out"
-              style={{
-                transform: isRegistering ? "rotateY(180deg)" : "rotateY(0deg)",
-              }}
-            >
-              {/* Form di Login */}
-              <div
-                className={`absolute inset-0 transition-all duration-500 ease-in-out ${
-                  isRegistering ? "opacity-0" : "opacity-100"
-                }`}
-                style={{ backfaceVisibility: "hidden" }}
-              >
-                <form onSubmit={handleAuth}>
-                  <h2 className="text-2xl font-bold mb-4">Login</h2>
-                  <div className="mb-4">
-                    <label className="block text-gray-700 mb-2">Email</label>
-                    <input
-                      type="email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      placeholder="Inserisci la tua email"
-                      className="border border-gray-300 rounded p-2 w-full"
-                      required
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <label className="block text-gray-700 mb-2">Password</label>
-                    <input
-                      type="password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      placeholder="Inserisci la tua password"
-                      className="border border-gray-300 rounded p-2 w-full"
-                      required
-                    />
-                  </div>
-                  <button
-                    type="submit"
-                    className="bg-blue-500 text-white py-2 px-4 rounded w-full hover:bg-blue-600"
-                  >
-                    Login
-                  </button>
-                  <button
-                    type="button"
-                    onClick={() => setIsRegistering(true)} // Passa a registrazione
-                    className="text-blue-500 mt-2"
-                  >
-                    Non hai un account? Registrati
-                  </button>
-                </form>
-              </div>
-
-              {/* Form di Registrazione */}
-              <div
-                className={`absolute inset-0 transition-all duration-500 ease-in-out ${
-                  isRegistering ? "opacity-100" : "opacity-0"
-                }`}
-                style={{ backfaceVisibility: "hidden" }}
-              >
-                <form onSubmit={handleAuth}>
-                  <h2 className="text-2xl font-bold mb-4">Registrati</h2>
-                  <div className="mb-4">
-                    <label className="block text-gray-700 mb-2">Email</label>
-                    <input
-                      type="email"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
-                      placeholder="Inserisci la tua email"
-                      className="border border-gray-300 rounded p-2 w-full"
-                      required
-                    />
-                  </div>
-                  <div className="mb-4">
-                    <label className="block text-gray-700 mb-2">Password</label>
-                    <input
-                      type="password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
-                      placeholder="Inserisci la tua password"
-                      className="border border-gray-300 rounded p-2 w-full"
-                      required
-                    />
-                  </div>
-                  <button
-                    type="submit"
-                    className="bg-blue-500 text-white py-2 px-4 rounded w-full hover:bg-blue-600"
-                  >
-                    Registrati
-                  </button>
-                  <button
-                    type="button"
-                    onClick={() => setIsRegistering(false)} // Torna al login
-                    className="text-blue-500 mt-2"
-                  >
-                    Hai già un account? Accedi
-                  </button>
-                </form>
-              </div>
+      {!user ? ( // Se l'utente non è loggato, mostra il form
+        <div className="bg-white p-8 rounded shadow-md w-80">
+          <form onSubmit={handleAuth}>
+            <h2 className="text-2xl font-bold mb-4">
+              {isRegistering ? "Registrati" : "Login"}
+            </h2>
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2">Email</label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)} // Aggiorna l'email
+                placeholder="Inserisci la tua email"
+                className="border border-gray-300 rounded p-2 w-full"
+                required
+              />
             </div>
-          </Flipped>
-        </Flipper>
+            <div className="mb-4">
+              <label className="block text-gray-700 mb-2">Password</label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)} // Aggiorna la password
+                placeholder="Inserisci la tua password"
+                className="border border-gray-300 rounded p-2 w-full"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-blue-500 text-white py-2 px-4 rounded w-full hover:bg-blue-600"
+            >
+              {isRegistering ? "Registrati" : "Accedi"}{" "}
+              {/* Cambia il testo del bottone */}
+            </button>
+            <button
+              type="button"
+              onClick={() => setIsRegistering(!isRegistering)} // Alterna tra login e registrazione
+              className="text-blue-500 mt-2"
+            >
+              {isRegistering
+                ? "Hai già un account? Accedi"
+                : "Non hai un account? Registrati"}
+            </button>
+          </form>
+        </div>
       ) : (
+        // Se l'utente è loggato, mostra il messaggio di benvenuto e il pulsante di logout
         <div className="flex flex-col items-center">
           <h2 className="text-2xl font-bold mb-4">
             Benvenuto, {user.email.split("@")[0]}
           </h2>
           <button
             onClick={handleLogout}
-            className="bg-green-500 text-white py-2 px-4 rounded hover:bg-green-600"
+            className="bg-red text-white py-2 px-4 rounded hover:bg-red"
           >
             Logout
           </button>
