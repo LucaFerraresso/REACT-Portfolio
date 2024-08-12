b4fbc5aef67a136a8e046105d5a1437a8fa2933c
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-08-12 18:01:40 +0200

    fix db

--- MyApp/server.js
+++ MyApp/server.js
@@ -3,34 +3,45 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 
+// Configurazione dotenv
+dotenv.config();
+console.log("MONGODB_URI", process.env.MONGODB_URI);
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Definizione dello schema per l'esercizio
 const exerciseSchema = new mongoose.Schema({
   title: String,
   description: String,
   type: String,
   difficulty: String,
 });
+
 const Exercise = mongoose.model("Exercise", exerciseSchema);
 
-dotenv.config();
-console.log("MONDODB_URI", process.env.MONGODB_URI);
-const app = express();
-const PORT = process.env.PORT || 5001;
-const MONGODB_URI = process.env.MONGODB_URI;
-app.use(cors());
-app.use(express.json());
-
+// Connessione a MongoDB utilizzando Mongoose
 mongoose
   .connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("MongoDB connected"))
+  .then(() => {
+    console.log("MongoDB connected");
+
+    // Avvio del server solo dopo una connessione avvenuta con successo
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
   .catch((err) => console.error("MongoDB connection error:", err));
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
+// Endpoint per recuperare gli esercizi
 app.get("/exercises", async (req, res) => {
   try {
     const exercises = await Exercise.find();
@@ -38,8 +49,4 @@ app.get("/exercises", async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
