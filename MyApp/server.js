8153cab18d8e3041dfe1b0386d0278f1d8987e69
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-08-12 17:32:39 +0200

    fix server.js

--- MyApp/server.js
+++ MyApp/server.js
@@ -1,12 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
+
+const exerciseSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  type: String,
+  difficulty: String,
+});
+const Exercise = mongoose.model("Exercise", exerciseSchema);
 
 dotenv.config();
 console.log("MONDODB_URI", process.env.MONGODB_URI);
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGODB_URI = process.env.MONGODB_URI;
+app.use(cors());
+app.use(express.json());
 
 mongoose
   .connect(MONGODB_URI, {
@@ -18,4 +29,17 @@ mongoose
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/exercises", async (req, res) => {
+  try {
+    const exercises = await Exercise.find();
+    res.json(exercises);
+  } catch (error) {
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
