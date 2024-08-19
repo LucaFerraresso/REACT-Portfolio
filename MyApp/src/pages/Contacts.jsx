bd8b6d0bc52c6bd9ddbb0ef18fde03f449c55e85
LucaFerraresso <156689909+LucaFerraresso@users.noreply.github.com>
2024-08-19 12:42:23 +0200

    fix contactpage design/style/color/animation

--- MyApp/src/pages/Contacts.jsx
+++ MyApp/src/pages/Contacts.jsx
@@ -1,12 +1,45 @@
 import React from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-//svg
+import { motion } from "framer-motion";
+// SVG Imports
 import LinkedInSvg from "../components/svg/LinkedInSvg";
 import FacebookSvg from "../components/svg/FacebookSvg";
 import GmailSvg from "../components/svg/GmailSvg";
 import GitHubSvg from "../components/svg/GitHubSvg";
 import TwitterSvg from "../components/svg/TwitterSvg";
+
+// Varianti di animazione
+const fadeIn = {
+  hidden: { opacity: 0, y: -20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 1.5 },
+  },
+};
+
+const staggerContainer = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.5,
+    },
+  },
+};
+
+const socialLinkVariants = {
+  hidden: { opacity: 0, scale: 0.9 },
+  visible: (custom) => ({
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+      delay: custom * 0.5,
+    },
+  }),
+};
 
 const ContactPage = () => {
   const handleSubmit = (e) => {
@@ -15,9 +48,17 @@ const ContactPage = () => {
   };
 
   return (
-    <div className="flex flex-row justify-center items-center lg: min-h-screen bg-main-bg p-6">
-      <div className="flex flex-col lg:flex-row items-center justify-center w-full lg:w-2/3 bg-white rounded-lg shadow-lg p-8">
-        <div className="flex flex-col w-full lg:w-1/2 mb-8 lg:mb-0">
+    <div className="flex justify-center items-center min-h-screen bg-main-bg p-6 font-montserrat">
+      <motion.div
+        className="flex flex-col lg:flex-row items-center justify-center w-full lg:w-2/3 bg-white rounded-lg shadow-lg p-8"
+        variants={staggerContainer}
+        initial="hidden"
+        animate="visible"
+      >
+        <motion.div
+          className="flex flex-col w-full lg:w-1/2 mb-8 lg:mb-0"
+          variants={fadeIn}
+        >
           <h2 className="text-2xl font-bold text-text-dark-desaturated-blue mb-4">
             Mettiti in contatto con me!
           </h2>
@@ -32,28 +73,35 @@ const ContactPage = () => {
             <p>Tel: 334 83 66 ###</p>
             <p>Email: lucaferraresso96@gmail.com</p>
           </div>
-          <div className="flex p-2 gap-2 justify-center space-x-4 mt-4 mb-4">
-            <a>
-              <LinkedInSvg />
-            </a>
-            <a>
-              <FacebookSvg />
-            </a>
-            <a>
-              <GmailSvg />
-            </a>
-            <a>
-              <GitHubSvg />
-            </a>
-            <a>
-              <TwitterSvg />
-            </a>
-          </div>
-        </div>
+          <motion.div
+            className="flex p-2 gap-2 justify-center space-x-4 mt-4 mb-4"
+            variants={staggerContainer}
+            initial="hidden"
+            animate="visible"
+          >
+            {[LinkedInSvg, FacebookSvg, GmailSvg, GitHubSvg, TwitterSvg].map(
+              (IconComponent, index) => (
+                <motion.a
+                  key={index}
+                  href="#"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  variants={socialLinkVariants}
+                  custom={index}
+                  whileHover={{ scale: 1.2 }}
+                  className="w-10 h-10 sm:w-12 sm:h-12 flex justify-center items-center rounded-full hover:bg-gray-300 transition-colors duration-300 "
+                >
+                  <IconComponent className="w-6 h-6 sm:w-8 sm:h-8" />
+                </motion.a>
+              )
+            )}
+          </motion.div>
+        </motion.div>
 
-        <form
+        <motion.form
           onSubmit={handleSubmit}
-          className="w-full lg:w-1/2 bg-white p-6 rounded-lg shadow-md"
+          className="w-full lg:w-1/2 bg-white p-6 rounded-lg"
+          variants={fadeIn}
         >
           <div className="mb-4">
             <label
@@ -128,14 +176,14 @@ const ContactPage = () => {
               className="shadow appearance-none border rounded w-full py-2 px-3 text-text-dark-desaturated-blue leading-tight focus:outline-none focus:shadow-outline"
             ></textarea>
           </div>
-          <button
+          <motion.button
             type="submit"
             className="w-full bg-purple hover:bg-gradient-to-r from-purple to-dark-grayish-blue text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition-all duration-300 ease-in-out"
           >
             Invia il messaggio
-          </button>
-        </form>
-      </div>
+          </motion.button>
+        </motion.form>
+      </motion.div>
 
       <ToastContainer />
     </div>
